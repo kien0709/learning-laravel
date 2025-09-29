09a2d5aa7eea0d14a45aae16e300794148a3c9e2
kien0709 <2211395@talnet.nl>
2025-09-29 12:22:27 +0200

    dynamische routes

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,43 @@ Route::get('/planets', function () {
     
     return view('planets', compact('planets'));
 });
+
+Route::get('/planets/{planet}', function ($planet) {
+    $allPlanets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+    
+    // Find the specific planet by name (case-insensitive)
+    $foundPlanet = null;
+    foreach ($allPlanets as $planetData) {
+        if (strtolower($planetData['name']) === strtolower($planet)) {
+            $foundPlanet = $planetData;
+            break;
+        }
+    }
+    
+    // If planet not found, return 404 or redirect
+    if (!$foundPlanet) {
+        abort(404, 'Planet not found');
+    }
+    
+    // Pass the single planet to the view
+    $planets = [$foundPlanet];
+    
+    return view('planets', compact('planets'));
+});
