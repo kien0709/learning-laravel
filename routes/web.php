646f094152c842e327663eee0e9eda969f98c5d3
kien0709 <2211395@talnet.nl>
2025-11-28 10:01:43 +0100

    merge conflict gefixt

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,10 @@
 
 use App\Http\Controllers\PlanetController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PlanetController;
 
 Route::get('/', function () {
     return redirect('/planets');
 });
 
-<<<<<<< HEAD
 Route::get('/planets', [PlanetController::class, 'index']);
 Route::get('/planets/{id}', [PlanetController::class, 'show'])->whereNumber('id');
-
-=======
-Route::get('/planeten', function () {
-    return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
-});
-
-Route::get('/planets', [PlanetController::class, 'index']);
-
-Route::get('/planets/{planet}', [PlanetController::class, 'show']);
->>>>>>> a9202bf96ded3f29e1693bcd4716164375b2f0a3
